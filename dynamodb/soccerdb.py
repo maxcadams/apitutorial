177a3f0c411451da1455a31f909c5c124c74484c
Max Adams <maxadams@c02z335blvdr.lan>
2022-06-01 17:31:42 -0400

    finished tests & dynamodb proto

--- dynamodb/soccerdb.py
+++ dynamodb/soccerdb.py
@@ -67,11 +67,11 @@ class Footy:
             self.table = self.dyn_resource.create_table(
                 TableName=table_name,
                 KeySchema=[
-                    {'AttributeName': 'club name', 'KeyType': 'HASH'},  # Partition key
+                    {'AttributeName': 'club_name', 'KeyType': 'HASH'},  # Partition key
                     {'AttributeName': 'league', 'KeyType': 'RANGE'}  # Sort key
                 ],
                 AttributeDefinitions=[
-                    {'AttributeName': 'club name', 'AttributeType': 'S'},
+                    {'AttributeName': 'club_name', 'AttributeType': 'S'},
                     {'AttributeName': 'league', 'AttributeType': 'S'}
                 ],
                 ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10})
@@ -91,22 +91,23 @@ class Footy:
         :param club_name: name of club
         :param league: league in which club resides
         :param wins: total number of wins in 2020/2021 season club had
-        :param total_revenue: revenue of club in 2020/2021 season
+        :param total_revenue: revenue of club in 2020/2021 season (millions, euros)
         """
         try:
-            self.table.put_item(
-                Item={
-                    'club name': club_name,
-                    'league': league,
-                    'info': { 'wins': wins, 'total-revenue': total_revenue}
-                }
-            )
+            response = self.table.put_item(
+                    Item={
+                        'club_name': club_name,
+                        'league': league,
+                        'info': { 'wins': wins, 'total_revenue': Decimal(str(total_revenue))}
+                    }
+                )
         except ClientError as err:
             logger.error(
                 "Couldn't add club %s to table %s. Here's why: %s: %s",
                 club_name, self.table.name,
                 err.response['Error']['Code'], err.response['Error']['Message'])
             raise
+        return response
     
     def update_item(self, club_name, league, wins, total_revenue):
         """
@@ -116,13 +117,14 @@ class Footy:
         :param league: league in which club resides
         :param wins: total number of wins in 2020/2021 season club had
         :param total_revenue: revenue of club in 2020/2021 season
+        :return: Updated fields & values
         """
         try:
             response = self.table.update_item(
                 Key={'club_name': club_name, 'league': league},
-                UpdateExpression="set info.wins=:w, info.total-revenue=:t",
+                UpdateExpression="set info.wins=:w, info.total_revenue=:t",
                 ExpressionAttributeValues={
-                    ':w': wins, ':t': total_revenue
+                    ':w': wins, ':t': Decimal(str(total_revenue))
                 },
                 ReturnValues="UPDATED_NEW")
         except ClientError as err:
@@ -131,8 +133,68 @@ class Footy:
                 club_name, self.table.name,
                 err.response['Error']['Code'], err.response['Error']['Message'])
             raise
+        return response['Attributes']
     
-    
+    def get_item(self, club_name, league):
+        """
+        Gets club from table given club name and league
+
+        :param club_name: club being queried
+        :return: List of clubs with that name (typically one)
+        """
+        try:
+            response = self.table.get_item(Key={'club_name': club_name, 'league': league})
+        except ClientError as err:
+            logger.error(
+                "Couldn't query for club %s. Here's why: %s: %s", year,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        return response['Item']
+
+    def get_item_helper(self, club_name, league):
+        """
+        Looks for club, club_name, in table. 
+        Used for testing purposes
+
+        :param club_name: club being queried
+        :return: True if club in table, False if not
+        """
+        response = self.table.get_item(Key={'club_name': club_name, 'league': league})
+        if 'Item' in response.keys():
+            return True
+        return False
+
+
+
+    def delete_item(self, club_name, league):
+        """
+        Deletes club_name, league entry from table
+
+        :param club_name: name of club
+        :param league: league in which club resides
+        """
+        try:
+            self.table.delete_item(Key={'club_name': club_name, 'league': league})
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete club %s. Here's why: %s: %s", club_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+    def delete_table(self):
+        """
+        Deletes table
+        """
+        try: 
+            self.table.delete()
+            self.table = None
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete table. Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+
 
     
 if __name__ == '__main__':
@@ -140,5 +202,8 @@ if __name__ == '__main__':
         dyn_resource = boto3.resource('dynamodb')
         footy = Footy(dyn_resource)
         table = footy.create_table('Clubs')
+        footy.add_item('Real Madrid', 'La Liga', 25, 640.7)
+        print(footy.get_item_helper('Real Madrid', 'La Liga'))
+        footy.delete_table()
     except Exception as e:
         print(f"Something went wrong w demo! Here's what: {e}")
--- dynamodb/tests/test_fns.py
+++ dynamodb/tests/test_fns.py
@@ -9,13 +9,71 @@ from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
 
 
+dyn = boto3.resource('dynamodb')
+db = soccerdb.Footy(dyn)
 
 
 def test_create_table():
-    dyn = boto3.resource('dynamodb')
+    #dyn = boto3.resource('dynamodb')
+    
     #initialize DB structure
-    db = soccerdb.Footy(dyn)
     table = db.create_table('Clubs')
     
     assert table.name == 'Clubs'
     assert table.table_status == 'CREATING'
+    assert db.table != None
+
+def test_add_item():
+    resp = db.add_item('Real Madrid', 'La Liga', 25, 640.7)
+    
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+    print(db.table.item_count)
+
+
+def test_update_item():
+    update = {'info': {
+        'wins': Decimal(23), 
+        'total_revenue': Decimal(str(650.6))
+    }}
+
+    update_res = db.update_item('Real Madrid', 'La Liga', 23, 650.6)
+
+    assert update == update_res
+
+
+def test_get_item():
+    in_club = {
+        'club_name': 'Real Madrid',
+        'league': 'La Liga',
+        'info': {
+            'wins': Decimal(23),
+            'total_revenue': Decimal(str(650.6))
+        }
+    }
+    
+    out_club = db.get_item(in_club['club_name'], in_club['league'])
+
+    assert in_club == out_club
+    assert db.get_item_helper(in_club['club_name'], in_club['league'])
+
+
+def test_delete_item():
+    del_key = {
+        'club_name': 'Real Madrid',
+        'league': 'La Liga'
+    }
+
+    db.delete_item(del_key['club_name'], del_key['league'])
+
+    assert not(db.get_item_helper(del_key['club_name'], del_key['league']))
+
+
+
+def test_delete_table():
+    db.delete_table()
+
+    #assert table.table_status == 'DELETING'
+    assert db.table == None
+
+
+
